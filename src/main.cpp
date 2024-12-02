99b28ccfde241a0400e8ac0f838a1b0bc18bafca
Karim Butmir <kariboter1@gmail.com>
2024-12-02 10:46:27 +0300

    lab02 is done

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,57 @@
 
 
 #include "karel.h"
+#include <iostream>
 
-
+void PickAllBeepers();
+void TurnRight();
 //==============================================================================
 /// Программа для робота Карела
 ///
 void KarelProgram() {
+    bool is_even = false;
 /// @todo Добавить программу для Карела
+    while(FrontIsClear()){
+        while(FrontIsClear()){
+            PickAllBeepers();
+            Move();
+        }
+        PickAllBeepers();
+        if(!is_even){
+            TurnLeft();
+            if(!FrontIsClear())
+                return;
+            Move();
+            TurnLeft();
+            
+        }
+        else{
+            TurnRight();
+            if(!FrontIsClear())
+                return;
+            Move();
+            TurnRight();
+        }
+        is_even = !is_even;
+        }
 
 }
 
+void PickAllBeepers(){
+    while (BeepersPresent())
+        PickBeeper();
+}
 
+void TurnRight(){
+    for(int i = 0; i < 3; i++){
+        TurnLeft();
+    }
+}
 //==============================================================================
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/collect_newspaper.w");
+    LoadWorld("worlds/CB_03.w");
     KarelProgram();
     Finish();
     return 0;
